8c71f70c1d7eceff8600f2610b0dda4bca717f9f
Márton Balogh <37423520+Marcikadraga@users.noreply.github.com>
2026-06-19 08:12:33 +0000

    Set up Angular app structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomePage } from './features/home/pages/home-page/home-page';
+import { LoginPage } from './features/auth/pages/login-page/login-page';
+import { RegisterPage } from './features/auth/pages/register-page/register-page';
+import { TodosPage } from './features/todos/pages/todos-page/todos-page';
+import { NotFoundPage } from './features/not-found/pages/not-found-page/not-found-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  },
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+  {
+    path: 'register',
+    component: RegisterPage,
+  },
+  {
+    path: 'todos',
+    component: TodosPage,
+  },
+  {
+    path: '**',
+    component: NotFoundPage,
+  },
+];
